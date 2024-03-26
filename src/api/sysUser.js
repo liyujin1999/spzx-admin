dd55999e9b606b65d8b4f90cf3a62e8c0a0b7a5e
yujin <yujinli0112@163.com>
2024-03-26 21:32:42 +0800

    新增用户管理功能(增删改查、头像上传minio)

---
+++ src/api/sysUser.js
@@ -0,0 +1,36 @@
+import request from '@/utils/request'
+
+// 分页查询
+export const GetSysUserListByPage = (pageNum , pageSize , queryDto) => {
+    return request({
+        url: `/admin/system/sysUser/findByPage/${pageNum}/${pageSize}`,
+        method: "get",
+        params: queryDto
+    })
+}
+
+// 新增用户的方法
+export const SaveSysUser = (data) => {
+    return request({
+        url: "/admin/system/sysUser/saveSysUser",
+        method: "post",
+        data: data,
+    })
+}
+
+// 修改用户数据的方法
+export const UpdateSysUser = (sysUser) => {
+    return request({
+        url: "/admin/system/sysUser/updateSysUser",
+        method: "put",
+        data: sysUser
+    })
+}
+
+// 根据id删除用户
+export const DeleteSysUserById = (userId) => {
+    return request({
+        url: `/admin/system/sysUser/deleteById/${userId}`,
+        method: 'delete',
+    })
+}
